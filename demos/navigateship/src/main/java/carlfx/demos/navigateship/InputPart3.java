627dd69cd17570a4d8b7bcf9a21d1d3061082464
Carl Dea <carldea@gmail.com>
2012-05-17 23:48:18 -0400

    Initial revision.

---
+++ demos/navigateship/src/main/java/carlfx/demos/navigateship/InputPart3.java
@@ -0,0 +1,33 @@
+package carlfx.demos.navigateship;
+
+import carlfx.gameengine.GameWorld;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The main driver of the game.
+ * @author cdea
+ */
+public class InputPart3 extends Application {
+   
+    GameWorld gameWorld = new TheExpanse(59, "JavaFX 2 GameTutorial Part 3 - Input");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        // setup title, scene, stats, controls, and actors.
+        gameWorld.initialize(primaryStage);
+
+        // kick off the game loop
+        gameWorld.beginGameLoop();
+
+        // display window
+        primaryStage.show();       
+    }
+
+}
