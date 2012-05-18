daf4aeaef09cad11110737d9526f057bee63dff1
Carl Dea <carldea@gmail.com>
2012-05-17 21:54:23 -0400

    Initial revision.

---
+++ demos/navigateship/src/main/java/carlfx/demos/navigateship/Missile.java
@@ -0,0 +1,12 @@
+package carlfx.demos.navigateship;
+
+import javafx.scene.paint.Color;
+
+/**
+ * A missile projectile without the radial gradient.
+ */
+public class Missile extends Atom {
+    public Missile(Color fill) {
+        super(5, fill, false);
+    }
+}
