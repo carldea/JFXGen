77b122fb619cb7415e9f1c14c38ab4f35757a13b
Carl Dea <carldea@gmail.com>
2012-06-09 17:08:58 -0400

    Made the getSpriteManager() method public. This allows sprites to remove themselves.

--- jfxgenproj/src/main/java/carlfx/gameengine/GameWorld.java
+++ jfxgenproj/src/main/java/carlfx/gameengine/GameWorld.java
@@ -22,32 +22,44 @@ import javafx.util.Duration;
  *  <b>checkCollisions()</b> - Method will determine objects that collide with each other.
  *  <b>cleanupSprites()</b> - Any sprite objects needing to be removed from play.
  * </pre>
+ *
  * @author cdea
  */
 public abstract class GameWorld {
-    
-    /** The JavaFX Scene as the game surface */
+
+    /**
+     * The JavaFX Scene as the game surface
+     */
     private Scene gameSurface;
-    /** All nodes to be displayed in the game window. */
+    /**
+     * All nodes to be displayed in the game window.
+     */
     private Group sceneNodes;
-    /** The game loop using JavaFX's <code>Timeline</code> API.*/
+    /**
+     * The game loop using JavaFX's <code>Timeline</code> API.
+     */
     private static Timeline gameLoop;
-    
-    /** Number of frames per second. */
+
+    /**
+     * Number of frames per second.
+     */
     private final int framesPerSecond;
-    
-    /** Title in the application window.*/
+
+    /**
+     * Title in the application window.
+     */
     private final String windowTitle;
 
     /**
      * The sprite manager.
      */
     private final SpriteManager spriteManager = new SpriteManager();
-  
+
     /**
-     * Constructor that is called by the derived class. This will 
+     * Constructor that is called by the derived class. This will
      * set the frames per second, title, and setup the game loop.
-     * @param fps - Frames per second.
+     *
+     * @param fps   - Frames per second.
      * @param title - Title of the application window.
      */
     public GameWorld(final int fps, final String title) {
@@ -56,85 +68,86 @@ public abstract class GameWorld {
         // create and set timeline for the game loop
         buildAndSetGameLoop();
     }
-    
+
     /**
      * Builds and sets the game loop ready to be started.
      */
     protected final void buildAndSetGameLoop() {
-        
-        final Duration oneFrameAmt = Duration.millis(1000/getFramesPerSecond());
-        final KeyFrame oneFrame = new KeyFrame(oneFrameAmt, 
-            new EventHandler<ActionEvent>() {
-                
-                @Override
-                public void handle(javafx.event.ActionEvent event) {
-                    
-                    // update actors
-                    updateSprites(); 
-  
-                    // check for collision
-                    checkCollisions();
-                    
-                    // removed dead things
-                    cleanupSprites();
-                                    
-                }
-        }); // oneFrame
-        
+
+        final Duration oneFrameAmt = Duration.millis(1000 / (float) getFramesPerSecond());
+        final KeyFrame oneFrame = new KeyFrame(oneFrameAmt,
+                new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(javafx.event.ActionEvent event) {
+
+                        // update actors
+                        updateSprites();
+
+                        // check for collision
+                        checkCollisions();
+
+                        // removed dead things
+                        cleanupSprites();
+
+                    }
+                }); // oneFrame
+
         // sets the game world's game loop (Timeline)
         setGameLoop(TimelineBuilder.create()
                 .cycleCount(Animation.INDEFINITE)
                 .keyFrames(oneFrame)
-                .build()); 
+                .build());
     }
-    
+
     /**
      * Initialize the game world by update the JavaFX Stage.
-     * @param primaryStage 
+     *
+     * @param primaryStage The main window containing the JavaFX Scene.
      */
     public abstract void initialize(final Stage primaryStage);
 
-    /**Kicks off (plays) the Timeline objects containing one key frame
+    /**
+     * Kicks off (plays) the Timeline objects containing one key frame
      * that simply runs indefinitely with each frame invoking a method
-     * to update sprite objects, check for collisions, and cleanup sprite 
+     * to update sprite objects, check for collisions, and cleanup sprite
      * objects.
-     * 
      */
     public void beginGameLoop() {
         getGameLoop().play();
-    } 
-    
+    }
+
     /**
-     * Updates each game sprite in the game world. This method will 
-     * loop through each sprite and passing it to the handleUpdate() 
+     * Updates each game sprite in the game world. This method will
+     * loop through each sprite and passing it to the handleUpdate()
      * method. The derived class should override handleUpdate() method.
-     * 
      */
     protected void updateSprites() {
-        for (Sprite sprite:spriteManager.getAllSprites()){
+        for (Sprite sprite : spriteManager.getAllSprites()) {
             handleUpdate(sprite);
         }
     }
-    
-    /** Updates the sprite object's information to position on the game surface.
+
+    /**
+     * Updates the sprite object's information to position on the game surface.
+     *
      * @param sprite - The sprite to update.
      */
     protected void handleUpdate(Sprite sprite) {
     }
-    
+
     /**
      * Checks each game sprite in the game world to determine a collision
      * occurred. The method will loop through each sprite and
      * passing it to the handleCollision()
      * method. The derived class should override handleCollision() method.
-     *
      */
     protected void checkCollisions() {
         // check other sprite's collisions
         spriteManager.resetCollisionsToCheck();
         // check each sprite against other sprite objects.
-        for (Sprite spriteA:spriteManager.getCollisionsToCheck()){
-            for (Sprite spriteB:spriteManager.getAllSprites()){
+        for (Sprite spriteA : spriteManager.getCollisionsToCheck()) {
+            for (Sprite spriteB : spriteManager.getAllSprites()) {
                 if (handleCollision(spriteA, spriteB)) {
                     // The break helps optimize the collisions
                     //  The break statement means one object only hits another
@@ -150,6 +163,7 @@ public abstract class GameWorld {
      * When two objects collide this method can handle the passed in sprite
      * objects. By default it returns false, meaning the objects do not
      * collide.
+     *
      * @param spriteA - called from checkCollision() method to be compared.
      * @param spriteB - called from checkCollision() method to be compared.
      * @return boolean True if the objects collided, otherwise false.
@@ -167,6 +181,7 @@ public abstract class GameWorld {
 
     /**
      * Returns the frames per second.
+     *
      * @return int The frames per second.
      */
     protected int getFramesPerSecond() {
@@ -175,6 +190,7 @@ public abstract class GameWorld {
 
     /**
      * Returns the game's window title.
+     *
      * @return String The game's window title.
      */
     public String getWindowTitle() {
@@ -184,8 +200,9 @@ public abstract class GameWorld {
     /**
      * The game loop (Timeline) which is used to update, check collisions, and
      * cleanup sprite objects at every interval (fps).
+     *
      * @return Timeline An animation running indefinitely representing the game
-     * loop.
+     *         loop.
      */
     protected static Timeline getGameLoop() {
         return gameLoop;
@@ -193,8 +210,9 @@ public abstract class GameWorld {
 
     /**
      * The sets the current game loop for this game world.
+     *
      * @param gameLoop Timeline object of an animation running indefinitely
-     * representing the game loop.
+     *                 representing the game loop.
      */
     protected static void setGameLoop(Timeline gameLoop) {
         GameWorld.gameLoop = gameLoop;
@@ -203,16 +221,18 @@ public abstract class GameWorld {
     /**
      * Returns the sprite manager containing the sprite objects to
      * manipulate in the game.
+     *
      * @return SpriteManager The sprite manager.
      */
-    protected SpriteManager getSpriteManager() {
+    public SpriteManager getSpriteManager() {
         return spriteManager;
     }
 
     /**
      * Returns the JavaFX Scene. This is called the game surface to
      * allow the developer to add JavaFX Node objects onto the Scene.
-     * @return
+     *
+     * @return Scene The JavaFX scene graph.
      */
     public Scene getGameSurface() {
         return gameSurface;
@@ -221,6 +241,7 @@ public abstract class GameWorld {
     /**
      * Sets the JavaFX Scene. This is called the game surface to
      * allow the developer to add JavaFX Node objects onto the Scene.
+     *
      * @param gameSurface The main game surface (JavaFX Scene).
      */
     protected void setGameSurface(Scene gameSurface) {
@@ -230,8 +251,9 @@ public abstract class GameWorld {
     /**
      * All JavaFX nodes which are rendered onto the game surface(Scene) is
      * a JavaFX Group object.
+     *
      * @return Group The root containing many child nodes to be displayed into
-     * the Scene area.
+     *         the Scene area.
      */
     public Group getSceneNodes() {
         return sceneNodes;
@@ -240,11 +262,12 @@ public abstract class GameWorld {
     /**
      * Sets the JavaFX Group that will hold all JavaFX nodes which are rendered
      * onto the game surface(Scene) is a JavaFX Group object.
+     *
      * @param sceneNodes The root container having many children nodes
-     * to be displayed into the Scene area.
+     *                   to be displayed into the Scene area.
      */
     protected void setSceneNodes(Group sceneNodes) {
         this.sceneNodes = sceneNodes;
     }
-    
+
 }
